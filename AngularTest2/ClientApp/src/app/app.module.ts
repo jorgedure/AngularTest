223e5a16d0b03d464edcbe7781b5a6b496da4d92
jorgedure <53130457+jorgedure@users.noreply.github.com>
2019-12-17 13:30:43 -0300

    update AngularTest2
    1. delete .net project
    2. add angular project

---
+++ AngularTest2/ClientApp/src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { IndexComponent } from './User/index/index.component';
+import { AddUserComponent } from './User/add-user/add-user.component';
+import { EditUserComponent } from './User/edit-user/edit-user.component';
+import { LoginComponent } from './login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import{DemoMaterialModule} from './DemoMaterialModule';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    IndexComponent,
+    AddUserComponent,
+    EditUserComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
+    DemoMaterialModule,
+    RouterModule.forRoot([
+      { path: '', component: LoginComponent, pathMatch: 'full' },
+      { path: 'add-user', component: AddUserComponent },
+      { path: 'edit-user', component: EditUserComponent },
+      { path: 'user', component: IndexComponent },
+    ]),
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
